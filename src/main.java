956bd765abadf39e8dbb5716f8c7c02615b05a93
fajar <febrimuayanah08@github.com>
2021-06-10 12:35:51 +0800

    Project IDEA

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
         String namaDepan = "Febri";
         String namaBelakang = "Muayanah";
+
         int usia = 20;
         int targetTahunKuliah = 4;
         double ipk = 3.89764512;
