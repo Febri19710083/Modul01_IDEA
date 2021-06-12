74bc9bcb38688f20f2967692d282bb037a345eed
fajar <febrimuayanah08@github.com>
2021-06-12 11:52:42 +0800

    Project IDEA

--- src/main.java
+++ src/main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         String namaDepan = "Febri";
         String namaBelakang = "Muayanah";
